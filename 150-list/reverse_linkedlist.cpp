adfcf1b1953a82bdc4fad7d8e0250005fce1ce01
j4999r <j4999r@protonmail.com>
2022-11-07 05:22:58 +0530

    reverse LL in cpp

---
+++ 150-list/reverse_linkedlist.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* cur = head;
+        ListNode* prev = NULL;
+        ListNode* tmp;
+        while (cur != NULL){
+            tmp = cur -> next;
+            cur -> next = prev;
+            prev = cur;
+            cur = tmp;
+        }
+        return prev;
+    }
+};
